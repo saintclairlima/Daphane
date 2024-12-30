5409b9f2175e7f17e08da4f4bfc4490835985b4a
Saint-Clair Lima <meuimeio.com>
2024-12-30 19:41:48 -0300

    Tenta novos ajustes para ver se conserta o histórico de mensagens

--- api/rag_chain.py
+++ api/rag_chain.py
@@ -103,14 +103,18 @@ Se você não souber a resposta, assuma um tom gentil e diga que não tem inform
 
     def consultar(self, dados_chat: DadosChat):
         pergunta = dados_chat['pergunta']
-        contexto = dados_chat['contexto']
+        contexto = [
+            ('human', "meu nome é Herman. Lembre-se disso")
+            ('assistant', "Claro! Vou me lembrar disso")
+        ]
+        #dados_chat['contexto']
         
         resultado = self.rag_chain.invoke({"pergunta": pergunta, 'contexto': contexto})
         
         resposta_llm = resultado["llm_response"]
         resposta_llm.response_metadata['message'] = str(resposta_llm.response_metadata['message'])
-        contexto.append(('human', pergunta))
-        contexto.append(('assistant', resposta_llm.content))
+        contexto.append(tuple('human', pergunta))
+        contexto.append(tuple('assistant', resposta_llm.content))
         
         return {'pergunta': pergunta,
                 'documentos': resultado["documentos_recuperados"],
